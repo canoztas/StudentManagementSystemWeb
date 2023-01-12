791e84d94b47d726230b6dc11d5e0bfeb270da80
eustache <canoztas06@gmail.com>
2023-01-12 16:25:42 +0000

    update

---
+++ lib/ui/lecture/lecturer_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:student_management_system_app/data/remote/service/grade_service.dart';
+
+import '../../data/remote/model/grade_model.dart';
+
+class LecturerPage extends StatefulWidget {
+  const LecturerPage({super.key});
+
+  @override
+  State<LecturerPage> createState() => _LecturerPageState();
+}
+
+class _LecturerPageState extends State<LecturerPage> {
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return FutureBuilder(
+      future: GradeService.service.fetchAllGrades(),
+      builder: (context, snapshot) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: SizedBox(
+            height: 500,
+            width: double.maxFinite,
+            child: Card(
+              elevation: 10,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("İSİM SOYİSİM",
+                            style: Theme.of(context).textTheme.displayMedium!.copyWith(fontWeight: FontWeight.bold)),
+                        Text("ÖĞRETİM ÜYESİ",
+                            style: Theme.of(context).textTheme.displayMedium!.copyWith(fontWeight: FontWeight.bold)),
+                      ],
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: snapshot.data?.length,
+                        itemBuilder: (context, index) {
+                          var grade = snapshot.data?[index];
+                          return buildListItem(grade ?? GradeModel());
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget buildListItem(GradeModel model) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 100,
+          width: 500,
+          child: Text(
+            model.grade?.lessonName ?? "",
+            style: Theme.of(context).textTheme.displaySmall,
+          ),
+        ),
+        SizedBox(
+          height: 100,
+          width: 500,
+          child: ListTile(
+            title: Text(
+              "İsim: ${model.lecturer?.firstName} ${model.lecturer?.lastName}",
+              style: Theme.of(context).textTheme.displaySmall,
+            ),
+            subtitle: Text(
+              "Mail: ${model.lecturer?.email}",
+              style: Theme.of(context).textTheme.displaySmall,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
